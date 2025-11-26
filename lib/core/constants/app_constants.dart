28202bcf7b28b1b7c9ac1899fef5c0d815e46568
osama-hawas <osama@gmail.com>
2025-11-26 03:45:42 +0200

    solve store problem

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String cairo = "cairo";
-  static const String appName = "راكان";
+  static const String appName = "EXP";
   static const String moyserApiKey = "pk_test_2SZLuDKsB4SoxsHwhAL46cn9Tg5S34NPLWba4gSW";
   static const String googleMapsKey = "AIzaSyCEMrdoAk7UaoEFCKbIEDtnZAGTsty_Jn4";
 }
